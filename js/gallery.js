183d86cd0338328a6effa07d61c87bac7f2575fb
Critzblank <jc675@njit.edu>
2017-04-04 20:36:09 -0400

    added GET parsing

--- js/gallery.js
+++ js/gallery.js
@@ -31,6 +31,18 @@ function animate() {
 }
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
+function getQueryParams(qs) {
+  qs = qs.split("+").join(" ");
+  var params = {}, tokens, re = /[?&]?([^=]+)=([^&]*)/g;
+  while (tokens = re.exec(qs)) {
+    params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+  }
+  return params;
+}
+
+var $_GET = getQueryParams(document.location.search);
+console.log($_GET["json"]);
+
 
 function swapPhoto() {
 
